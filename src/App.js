d23029ed883641f19302d870e6e5f3b8e2bd4b89
Jon Lee <39420293+4ka0@users.noreply.github.com>
2023-06-23 17:42:31 +0900

    State handling added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-function Square({ value }) {
-  return <button className="square">{ value }</button>
+import { useState } from "react";
+
+function Square() {
+
+  // This is a state variable.
+  // "value" stores the value.
+  // "setValue" is a function that can be used to change the value.
+  // "null" passed to useState is the initial value for this state variable.
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue("X")
+  }
+
+  return (
+    <button
+      className="square"
+      onClick={ handleClick }
+    >
+        { value }
+    </button>
+  )
 }
 
 export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square value="1"/>
-        <Square value="2"/>
-        <Square value="3"/>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="4"/>
-        <Square value="5"/>
-        <Square value="6"/>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="7"/>
-        <Square value="8"/>
-        <Square value="9"/>
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
