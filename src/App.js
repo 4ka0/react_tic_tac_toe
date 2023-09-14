0d58a4fd9fe61025b05a85b81187c1c04a39cd48
Jon Lee <39420293+4ka0@users.noreply.github.com>
2023-09-14 13:03:53 +0900

    Winner calculated

--- src/App.js
+++ src/App.js
@@ -14,18 +14,40 @@ function Square({ value, onSquareClick }) {
 
 export default function Board() {
 
+  // State boolean for determing whether O or X is next.
+  const [xIsNext, setXIsNext] = useState(true);
+
   // As an initial value, an array is created having nine elements each set to null.
   const [squares, setSquares] = useState( Array(9).fill( null ) );
 
   // Event handler for when a square is slicked.
   function handleClick(i) {
+    // If the elem is not null, it is already O/X, and thus don't continue.
+    if (squares[i]) {
+      return;
+    }
     const nextSquares = squares.slice();  // Creates copy of the squares array.
-    nextSquares[i] = "X";
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
+
+  // Check to see if there is a winner and display status.
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
   }
 
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         {/* The value in each Square is an elem index in the squares array.
             The same elem index is passed to the click event handler. */}
@@ -45,4 +67,25 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
